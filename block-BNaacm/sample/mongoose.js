17b6f8d520b2f2e192d82f23d939d39fbd3a82ff
ravindrapv <ravindra.ravi4849@gmail.com>
2022-07-12 23:11:12 +0530

    module

---
+++ block-BNaacm/sample/mongoose.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+
+var userSchema =  mongoose.Schema;
+
+
+var userData = new Schema({
+    name: {type:String,required:true},
+    age: {type:Number,default:18},
+    email:{type:String,lowercase:true,trime:true},
+    password:{type:String,minlength:5,maxlength:15}
+})
+
+var Article = mongoose.model('Article',userData);
+
+module.exports = Article;
